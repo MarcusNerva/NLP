67a310799694607c9fd8a3ad1d3fd5c89bc828ea
MarcusNerva <hadrianus_1@163.com>
2020-12-06 22:26:43 +0800

    feat: add start_testing.sh and start_training.sh

---
+++ myscripts/start_testing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/hanhuaye/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/hanhuaye/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/hanhuaye/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/hanhuaye/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+conda activate py37
+
+nohup python /home/hanhuaye/PythonProject/NLP/test.py --is_bilstm > \
+    /home/hanhuaye/PythonProject/NLP/myscripts/test.log \
+    2>&1 &
