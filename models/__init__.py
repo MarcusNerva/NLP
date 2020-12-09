760211bd4eda60336d83f759616eb7ba0e59b8ee
MarcusNerva <hadrianus_1@163.com>
2020-12-09 18:52:09 +0800

    feat: implement final_text_classifier.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 from .text_classifier import TextClassifierLSTM, TextClassifierTransformer
 from .visualize import Visualizer
 from .evaluation import eval
+from .final_text_classifier import Application_Classifier
--- models/final_text_classifier.py
+++ models/final_text_classifier.py
@@ -41,13 +41,4 @@ class Application_Classifier:
         out = torch.argmax(out, dim=1).item()
         return self.news_type[out]
 
-if __name__ == '__main__':
-    import sys
-    sys.path.append('../')
-    from cfgs import get_total_settings
-    args = get_total_settings()
-    model = Application_Classifier(args)
-    text = '北京时间12月9日凌晨，欧冠第6轮小组赛中，巴萨主场0-3负于尤文图斯，尤文获得头名出线，巴萨小组第2，欧冠主场不败金身告破。\
-    C罗罚中自己创造的点球，欧冠生涯首次攻破巴萨球门，下半时点球梅开二度，近13场做客诺坎普斩获14球，\
-    麦肯尼凌空抽射破门，连续两场建功，博努奇进球被判无效。'
-    print(model.classify(text))
+
