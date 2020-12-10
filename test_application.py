bac37c082bc39565318c4b3d3ca189884e07f6a9
MarcusNerva <hadrianus_1@163.com>
2020-12-10 09:57:24 +0800

    refactor: modified test_application.py

--- test_application.py
+++ test_application.py
@@ -1,4 +1,4 @@
-from models import BiLSTMClassifier
+from models import BiLSTMClassifier, TransformerClassifier
 
 if __name__ == '__main__':
     import sys
@@ -6,7 +6,14 @@ if __name__ == '__main__':
     from cfgs import get_total_settings
     args = get_total_settings()
     model = BiLSTMClassifier(args)
+    t_model = TransformerClassifier(args)
     text = '张新军蔡启煌麓山并列领先 记分卡直播决赛轮新浪体育讯　\
     9月4日-7日，2008年欧米茄中巡赛麓山锦标赛将在成都麓山国际高尔夫俱乐部举行。\
     共有112人报名参加本站赛事，其中包括国家队7位队员在内的10位业余球员。'
+
+    t_text = '''ApacheTomcat是美国阿帕奇（Apache）基金会的一款轻量级Web应用服务器。该程序实现了对Servlet和JavaServerPage（JSP）的支持。
+    ApacheTomcat存在安全漏洞，该漏洞源于可以重用HTTP2连接上接收到的前一个流的HTTP请求头值，
+    用于与后续流相关联的请求。虽然这很可能会导致错误和HTTP2连接的关闭，但信息可能会在请求之间泄漏。'''
     print(model.classify(text))
+
+    print(t_model.classify(t_text))
